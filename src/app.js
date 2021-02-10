24e5b167eeb27f63ede7a0fe4a99041d5d734361
akashsachithanandham <46894142+akashsachithanandham@users.noreply.github.com>
2021-02-10 07:50:29 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Weather from './Weather';
+class App extends Component {
+  //state = {  }
+  render() {
+    return (
+      <div >
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
